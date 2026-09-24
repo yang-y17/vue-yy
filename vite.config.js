0b80ffaaeaaa0495658ba07cc2878192db3ba506
yy-d 17 <2461480007@qq.com>
2026-09-24 20:51:35 +0800

    333

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: '/vue-yy/' ,
+  base: '/vue-yy/' ,
   plugins: [vue()],
+  build: {
+    rolldownOptions:{
+      output:{
+        entryFileNames: 'assets/[name].[hash].js',
+        chunkFileNames: 'assets/[name].[hash].js',
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
   
 })
